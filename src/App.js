6debd162ba1d7de17dbe2ea7c65285763e81b6c7
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-09-13 00:35:55 -0300

    Setup react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        :)
+        ;)
+        <Route exact path="/" render={
+          () => <div>'root!'</div>
+        } />
+        <Route exact path="/search" render={
+          () => <div>'search!'</div>
+        } />
       </div>
     );
   }
-}
+};
 
-export default App;
+
+const Routes = () =>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+;
+
+
+export default Routes;
