0ef644afa9b26823242e700ac7596a09fe931d1f
Rodolfo Stangherlin <rodolfo.3@gmail.com>
2017-09-21 01:58:03 -0300

    [wip] Add style for Book (including menu) and Shelf

--- src/Book.js
+++ src/Book.js
@@ -2,46 +2,100 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { SHELFS, SHELF_NAME } from './config';
 
+import './Book.css';
 
 const BookAction = ({ currentShelf, shelf, add }) => {
   if (currentShelf === shelf) {
-    return <span>{ SHELF_NAME[currentShelf] }</span>;
+    return <span className="book-current-shelf">{ SHELF_NAME[currentShelf] }</span>;
   } else {
-    return <button onClick={add}>{ SHELF_NAME[currentShelf] }</button>;
-  }
-};
-
-
-const BookMenu = ({ id, shelf, addToShelf }) =>
-  <span>
-    (
-    {
-      SHELFS.map(s =>
-        <BookAction
-          key={s}
-          currentShelf={s}
-          shelf={shelf}
-          add={() => addToShelf({ id, shelf: s })}
-        />
-      )
-    }
-    )
-  </span>
-;
-
-
-class Book extends Component {
-  render() {
-    const { title, id, shelf } = this.props;
     return (
-      <div>
-        { title }
-        <BookMenu id={id} shelf={shelf} addToShelf={this.context.addToShelf} />
-      </div>
+      <button onClick={add} className="book-action-btn">
+        { SHELF_NAME[currentShelf] }
+      </button>
     );
   }
 };
 
+
+const BookOptions = ({ id, shelf, addToShelf }) =>
+  <ul className="book-menu">
+    {
+      SHELFS.map(s =>
+        <li key={s}>
+          <BookAction
+            currentShelf={s}
+            shelf={shelf}
+            add={() => addToShelf({ id, shelf: s })}
+          />
+        </li>
+      )
+    }
+  </ul>
+;
+
+
+class BookMenu extends Component {
+  state = {
+    visible: false,
+  }
+
+  constructor(p) {
+    super();
+    this.open = this.open.bind(this);
+  }
+
+  open() {
+    this.setState({visible: true});
+  }
+
+  render() {
+    if (this.state.visible) {
+      return <BookOptions {...this.props} />;
+    }
+
+    return (
+      <button
+        onClick={this.open}
+        className="book-menu-button"
+      >
+        menu
+      </button>
+    );
+  }
+}
+
+
+
+class Book extends Component {
+  constructor() {
+    super();
+    this.addToShelf = this.addToShelf.bind(this);
+  }
+
+  addToShelf({id, shelf}) {
+    this.context.addToShelf({id, shelf});
+  }
+
+  render() {
+    const { title, authors, imageLinks } = this.props;
+    const { id, shelf } = this.props;
+
+    return (
+      <div className="book-container">
+        <div>
+          <img src={imageLinks.thumbnail} alt={title} />
+        </div>
+        <h3>
+          { title }
+        </h3>
+        <p className="book-author">
+          { authors.join(', ')}
+        </p>
+        <BookMenu id={id} shelf={shelf} addToShelf={this.addToShelf} />
+      </div>
+    );
+  }
+};
 Book.contextTypes = {
   addToShelf: PropTypes.func,
 }
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Book from './Book';
 
+import './Shelf.css';
+
 
 const Shelf = ({ books, title }) =>
   <div>
     <h2>{ title }</h2>
-    { books.map(book => <Book key={book.id} {...book} />) }
+    <ul className="shelf-list">
+      {
+        books.map(
+          book => (
+            <li key={book.id} className="shelf-item">
+              <Book key={book.id} {...book} />
+            </li>
+          )
+        )
+      }
+    </ul>
   </div>
 ;
 
